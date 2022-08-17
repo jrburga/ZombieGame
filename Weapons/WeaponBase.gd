194b67d2e38bddb754c84a99076f391fbc4a57b0
Jake Burga <jakerburga@gmail.com>
2022-08-16 22:44:54 -0400

    Imported sprite sheet, hook up animations (rough and quick)

--- Weapons/WeaponBase.gd
+++ Weapons/WeaponBase.gd
@@ -1,7 +1,11 @@
 extends Node2D
 class_name WeaponBase
 
+
 export(bool) var one_shot = true
+var trigger_pressed = false
+var reloading = false
+
 
 onready var bullet_spawner = find_node("BulletSpawner")
 
@@ -17,16 +21,37 @@ func _unhandled_input(event):
 			trigger_primary_pressed()
 		if event.is_action_released("attack_primary"):
 			trigger_primary_released()
+		if event.is_action_pressed("weapon_reload"):
+			reload_pressed()
 	
-var trigger_pressed = false
+
 func trigger_primary_pressed():
 	trigger_pressed = true
+	
+	if reloading:
+		return
+		
 	while trigger_pressed:
 		if bullet_spawner:
 			bullet_spawner.spawn_bullets(get_global_mouse_position())
 		if one_shot:
 			break
 		yield(bullet_spawner.find_node("TimerCooldown"), "timeout")
+		
+func reload_pressed():
+	if trigger_pressed:
+		return
+		
+	if reloading:
+		return
+		
+	
+	reloading = true
+	for frame in range(1, $Sprite.hframes):
+		$Sprite.frame = frame
+		yield(get_tree().create_timer(.05), "timeout")
+	$Sprite.frame = 0
+	reloading = false
 
 func trigger_primary_released():
 	trigger_pressed = false
