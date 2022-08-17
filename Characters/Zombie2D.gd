9384d0eb036aa03abf17ea818e57a74419a2f296
Jake Burga <jakerburga@gmail.com>
2022-08-17 00:34:23 -0400

    Add health to zombies

--- Characters/Zombie2D.gd
+++ Characters/Zombie2D.gd
@@ -3,6 +3,7 @@ extends KinematicBody2D
 
 export(float) var speed : float = 10
 var character = null
+var dying = false
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	character = get_node('/root').find_node('Character2D', true, false)
@@ -14,11 +15,22 @@ func _ready():
 #	pass
 
 func _physics_process(delta):
+	if dying:
+		return
+		
 	if character:
 		var delta_position = character.global_position - global_position
-		
 		var direction = delta_position.normalized()
-		
 		var velocity = direction * speed
-		
 		move_and_slide(velocity)
+
+func begin_die():
+	dying = true
+	for n in range(10):
+		modulate.a *= .8
+		yield(get_tree().create_timer(.1), "timeout")
+	queue_free()
+
+func _on_HealthNode_on_current_value_changed(prev_value, current_value, value_node):
+	if current_value == 0:
+		begin_die()
--- Weapons/Bullet.gd
+++ Weapons/Bullet.gd
@@ -15,13 +15,17 @@ func _physics_process(delta):
 	
 	if $RayCast2D.is_colliding():
 		global_position = $RayCast2D.get_collision_point()
+		var hurt_area = $RayCast2D.get_collider() as Area2D
 		$RayCast2D.enabled = false
-		hit_hurt_area()
+		hit_hurt_area(hurt_area)
 	
 func _on_Timer_timeout():
 	queue_free()
 
-func hit_hurt_area():
+func hit_hurt_area(hurt_area : Node2D):
+	if hurt_area:
+		var health_node = hurt_area.owner.find_node("HealthNode") as ValueNode
+		health_node.update_value(-10)
 	velocity = Vector2(0, 0)
 	$Bullet.visible = false
 	$Timer.start(0.25)
