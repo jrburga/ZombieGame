452d2c359a194d17f53d067b828ca0a842a534c1
Jake Burga <jakerburga@gmail.com>
2022-08-20 22:47:33 -0400

    mess around with room lighting

--- Rooms/Room.gd
+++ Rooms/Room.gd
@@ -1,7 +1,15 @@
 extends Area2D
+class_name RoomArea2D
 
 func _ready():
 	connect("area_entered", self, "_on_area_entered")
 	
 func _on_area_entered(area : Area2D):
-	print(area.owner)
+	if area.owner is Character2D:
+		print(area.owner)
+
+func activate_room():
+	find_node("Light2D").enabled = true
+	
+func deactivate_room():
+	find_node("Light2D").enabled = false
