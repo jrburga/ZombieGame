8d8427b71ca5eb3aeab34c79ce79d77df2056d09
Jake Burga <jakerburga@gmail.com>
2022-08-24 23:01:13 -0400

    make light transitions smoother

--- Rooms/Room.gd
+++ Rooms/Room.gd
@@ -1,8 +1,30 @@
 extends Area2D
 class_name RoomArea2D
 
-func activate_room():
-	find_node("Light2D").enabled = true
+func activate_room(immediate = false):
+	var light = find_node("Light2D")
 	
-func deactivate_room():
-	find_node("Light2D").enabled = false
+
+	if immediate:
+		light.color = Color.white
+		light.enabled = true
+		return
+
+	light.color = Color.black
+	light.enabled = true
+	var tween = create_tween()
+	tween.tween_property(light, "color", Color.white, .25)
+	
+	
+func deactivate_room(immediate = false):
+	var light = find_node("Light2D")
+	
+	light.color = Color.white
+	
+	if immediate:
+		light.enabled = false
+		return
+		
+	var tween = create_tween()
+	tween.tween_property(light, "color", Color.black, .25)
+	tween.tween_property(light, "enabled", false, 0)
--- Rooms/Rooms.gd
+++ Rooms/Rooms.gd
@@ -7,32 +7,14 @@ var entering_room = null
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	for child in get_children():
-		var room = child as Area2D
-		
-		room.deactivate_room()
-		room.connect("area_entered", self, "_on_area_entered", [room])
-		room.connect("area_exited", self, "_on_area_exited", [room])
-		
+		var room = child as RoomArea2D
+		room.deactivate_room(true)
 		room.connect("body_entered", self, "_on_body_entered", [room])
 		room.connect("body_exited", self, "_on_body_exited", [room])
-		
-func _on_area_entered(area : Area2D, room : Area2D):
-	print(area.owner)
-	if area.owner is Character2D:
-		if current_room == null:
-			current_room = room
-			current_room.activate_room()
-		else:
-			entering_room = room
-	
-func _on_area_exited(area : Area2D, exiting_room : Area2D):
-	if area.owner is Character2D:
-		if entering_room and current_room == exiting_room:
-			current_room.deactivate_room()
-			current_room = entering_room
-			current_room.activate_room()
-	entering_room = null
 
+
+		
+		
 func _on_body_entered(body : PhysicsBody2D, room : Area2D):
 	if body is Character2D:
 		if current_room == null:
