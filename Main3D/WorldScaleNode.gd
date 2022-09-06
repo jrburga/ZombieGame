c9d6086bf3bd5f12fb709f7cbafdbe05efd82f63
Jake Burga <jakerburga@gmail.com>
2022-09-05 23:25:55 -0400

    world scale, camera angle, 45 degrees

--- Main3D/WorldScaleNode.gd
+++ Main3D/WorldScaleNode.gd
@@ -1,10 +1,14 @@
+tool
 extends Spatial
 
-export(Vector3) var world_scale = Vector3(1, 1, 1)
+export(Vector3) var world_scale = Vector3(1, 1, 1) setget _on_set_world_scale
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
 
+func _on_set_world_scale(value):
+	world_scale = value
+	scale = world_scale
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
